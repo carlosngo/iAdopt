836e70167a8626aa12e5b6b3498b7c2aa15ae07e
johanna <johanna_lim@dlsu.edu.ph>
2019-08-01 21:44:00 +0800

    update

--- model/news.js
+++ model/news.js
@@ -1,24 +1,43 @@
 const db = require("./db.js");
 const database = db.database;
 
-function Create(/* parameters */) {
-
+function Create(newsPost, callback) {
+    let id = database.ref('posts/').push().key;
+    database.ref('posts/' + id).set({
+        "content": newsPost.content,
+        "time-posted": newsPost.time-posted,
+        "author": newsPost.author,
+        "picture": newsPost.picture
+    }, (err) => {
+        callback(err);
+    });
 }
 
-function RetrieveOne(/* parameters */) {
-
+function RetrieveOne(postId, callback) {
+    database.ref('posts/' + postId).once('value').then(function(snapshot) {
+        callback(snapshot.val())
+    });
 }
 
-function RetrieveAll(/* parameters */) {
-
+function RetrieveAll(callback) {
+    database.ref('posts').once('value').then(function(snapshot) {
+        callback(snapshot.val());
+    })
 }
 
-function Update(/* params */) {
 
+function Update(postId, newData, callback) {
+    var updates = {};
+    updates['/posts/' + postId] = newData;
+    database.ref().update(updates, (err) => {
+        callback(err);
+    })
 }
 
-function Delete(/* params */) {
-
+function Delete(postId, callback) {
+    database.ref('posts/' + postId).remove((err) => {
+        callback(err);
+    })
 }
 
 module.exports = {
--- model/users.js
+++ model/users.js
@@ -1,24 +1,41 @@
 const db = require("./db.js");
 const database = db.database;
 
-function Create(/* parameters */) {
-
+function Create(user, callback) {
+    let id = database.ref('users/').push().key;
+    database.ref('users/' + id).set({
+        "email": user.email,
+        "username": user.username,
+        "password": user.password
+    }, (err) => {
+        callback(err);
+    });
 }
 
-function RetrieveOne(/* parameters */) {
-
+function RetrieveOne(userId, callback) {
+    database.ref('users/' + userId).once('value').then(function(snapshot) {
+        callback(snapshot.val())
+    });
 }
 
-function RetrieveAll(/* parameters */) {
-
+function RetrieveAll(callback) {
+    database.ref('users').once('value').then(function(snapshot) {
+        callback(snapshot.val());
+    })
 }
 
-function Update(/* params */) {
-
+function Update(userId, newData, callback) {
+    var updates = {};
+    updates['/users/' + userId] = newData;
+    database.ref().update(updates, (err) => {
+        callback(err);
+    })
 }
 
-function Delete(/* params */) {
-
+function Delete(userId, callback) {
+    database.ref('users/' + userId).remove((err) => {
+        callback(err);
+    })
 }
 
 module.exports = {
