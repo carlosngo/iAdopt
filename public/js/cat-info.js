bb5f65df51a607bfb8aae86c54ceef9e5d8a70cd
Carlos <carlos_ngo@dlsu.edu.ph>
2019-08-04 19:06:43 +0800

    tweaked edit-cat

--- public/js/cat-info.js
+++ public/js/cat-info.js
@@ -1,5 +1,20 @@
 $(document).ready(() => {
+    $(".toggle-edit").hide();
     $("#back-btn").on('click', function() {
         window.history.back();
+    })
+
+    $("#btn-edit-cat").on('click', function() {
+        // $(".label-field").attr('class', 'editable-field');
+        $("#info-header").val("Editing cat:");
+        $(".toggle-show").hide();
+        $(".toggle-edit").show();
+    })
+
+    $("#btn-cancel-edit").on('click', function() {
+        
+        $("#info-header").val("Hi, my name is:");
+        $(".toggle-show").show();
+        $(".toggle-edit").hide();
     })
 })
