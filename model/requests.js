7e8484e099fc6b3dd0066b2fb33c9c50e81f2591
johanna <johanna_lim@dlsu.edu.ph>
2019-08-04 18:15:13 +0800

    update

--- model/requests.js
+++ model/requests.js
@@ -1,24 +1,42 @@
 const db = require("./db.js");
 const database = db.database;
 
-function Create() {
-
+function Create(userRequest, callback) {
+    let id = database.ref('requests/').push().key;
+    database.ref('requests/' + id).set({
+        "user": userRequest.user,
+        "content": userRequest.content,
+        "status": userRequest.status
+    }, (err) => {
+        callback(err);
+    });
 }
 
-function RetrieveOne() {
-
+function RetrieveOne(requestId, callback) {
+    database.ref('requests/' + requestId).once('value').then(function(snapshot) {
+        callback(snapshot.val())
+    });
 }
 
-function RetrieveAll() {
-
+function RetrieveAll(callback) {
+    database.ref('requests').once('value').then(function(snapshot) {
+        callback(snapshot.val());
+    })
 }
 
-function Update() {
 
+function Update(requestId, newData, callback) {
+    var updates = {};
+    updates['/requests/' + requestId] = newData;
+    database.ref().update(updates, (err) => {
+        callback(err);
+    })
 }
 
-function Delete() {
-    
+function Delete(requestId, callback) {
+    database.ref('requests/' + requestId).remove((err) => {
+        callback(err);
+    })
 }
 
 module.exports = {
