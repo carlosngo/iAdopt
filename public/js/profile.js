fc4fe8156ae7920cbe614c48b6a3b7169dba668a
johanna <johanna_lim@dlsu.edu.ph>
2019-08-06 20:26:51 +0800

    update

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,25 +1,29 @@
 $(document).ready(() => {
-    $("#ageOptions").val($("#age").text())
-    $("#sexOptions").val($("#gender").text())
-    $("#locationOptions").val($("#location").text())
-    $("#furPatternOptions").val($("#furPattern").text())
-    $("#ageOptions").val($("#age").text())
+    
     let state = $("#state").val();
     if (state === "edit") $(".toggle-show").hide();
     else $(".toggle-edit").hide();
     $("#back-btn").on('click', function() {
         window.history.back();
     })
-    $("#btn-edit-cat").on('click', function() {
+    $("#contEdit").on('click', function() {
         // $(".label-field").attr('class', 'editable-field');
-        $("#info-header").text("Editing cat:");
+        $("#profileheader").text("Edit Profile");
         $(".toggle-show").hide();
         $(".toggle-edit").show();
     })
 
-    $("#btn-cancel-edit").on('click', function() {
-        
-        $("#info-header").val("Hi, my name is:");
+    $("#editCancelBtn").on('click', function() {
+        $("#profileheader").val("Profile");
+        $(".toggle-show").show();
+        $(".toggle-edit").hide();
+    })
+
+    $("#saveBtn").on('click', function() {
+        /*
+            somethings should be here
+        */
+        $("#profileheader").val("Profile");
         $(".toggle-show").show();
         $(".toggle-edit").hide();
     })
