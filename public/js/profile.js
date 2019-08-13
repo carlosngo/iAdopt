cbaff45aff852671e4e00f45c40ea62d7e698ba6
Kagero1 <charles_stepven_lin@dlsu.edu.ph>
2019-08-13 20:19:05 +0800

    Profile backend workin

--- public/js/profile.js
+++ public/js/profile.js
@@ -21,9 +21,50 @@ $(document).ready(() => {
     })
 
     $("#saveBtn").on('click', function() {
+        console.log("I am here");
+        let email = $("#changeEmail").val();
+        let un = $("#changeUsername").val();
+        let pw = $("#changePassword").val();
+        let cpw = $("#changeCPassword").val();
+
+        console.log(email+un+pw+cpw);
+
+        if(email != "" && un != "" && pw != "" && cpw != ""){
+            console.log(email+un+pw+cpw);
+            if(pw == cpw){
+                console.log(email+un+pw+cpw);
+                $.ajax({
+                    url: "updateUser",
+                    method: "POST",
+                    data: {
+                        un: un,
+                        pw: pw,
+                        email: email
+                    },
+                    success: function() {
+                        $(".toggle-show").show();
+                        $(".toggle-edit").hide();
+                        document.location.reload();
+                    }                    
+                })
+            }else{
+                if($(".editError").length == 0){
+                    $(".editErrorMessage").append("<label class='editError' style='color: red;'>Wrong Repeated Password</label>");
+                }else{
+                    $(".editErrorMessage .editError").remove();
+                    $(".editErrorMessage").append("<label class='editError' style='color: red;'>Wrong Repeated Password</label>");
+                }
+            }
+
+        }else{
+            if($(".editError").length == 0){
+                    $(".editErrorMessage").append("<label class='editError' style='color: red;'>Fields cannot be Empty</label>");
+                }else{
+                    $(".editErrorMessage .editError").remove();
+                    $(".editErrorMessage").append("<label class='editError' style='color: red;'>Fields cannot be Empty</label>");
+                }
+        }
         $("#profileheader").text("Profile");
-        $(".toggle-show").show();
-        $(".toggle-edit").hide();
     })
 
     $("#pwConfirmationCancel").on('click', function() {
